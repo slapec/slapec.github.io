eb8fad9cd6ef8ac79f953e4d4a3044d69cbe6cc5
Slapec <slapec@gmail.com>
2019-02-18 21:20:37 +0100

    still trying

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,15 @@
 <template>
-    <h1>It Works!</h1>
+    <main>
+        <h1>Slapec weboldala</h1>
+        <p>
+            Úr Isten, úgy érzem most magam, mint valamikor anno 2003-ban, amikor elindítottam az első FrontPage Expressben
+            gyártott weboldalamat. Isten áldja a Windows 98 első kiadást!
+        </p>
+        <p>
+            Nem igérek semmit, hogy mi és mikor lesz itt.
+        </p>
+        <p>
+            slapec - 2019
+        </p>
+    </main>
 </template>
